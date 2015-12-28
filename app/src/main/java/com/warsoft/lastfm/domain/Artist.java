84bade7ffc3e65543d5b779004ede2f81bf483e0
xlreconlx <andersonur@hotmail.com>
2015-12-27 22:18:38 -0500

    agregando la nueva libreria y dependencias

--- app/src/main/java/com/warsoft/lastfm/domain/Artist.java
+++ app/src/main/java/com/warsoft/lastfm/domain/Artist.java
@@ -1,10 +1,17 @@
 package com.warsoft.lastfm.domain;
 
+import com.google.gson.annotations.SerializedName;
+import com.warsoft.lastfm.io.model.JsonKeys;
+
 /**
  * Created by ander on 26/12/2015.
  */
 public class Artist {
+    @SerializedName(JsonKeys.ARTIST_NAME)
     String name;
+
+    String urlLargeImage;
+    String urlMediumImage;
 
     public Artist(String name) {
         this.name = name;
@@ -16,5 +23,21 @@ public class Artist {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUrlLargeImage() {
+        return urlLargeImage;
+    }
+
+    public void setUrlLargeImage(String urlLargeImage) {
+        this.urlLargeImage = urlLargeImage;
+    }
+
+    public String getUrlMediumImage() {
+        return urlMediumImage;
+    }
+
+    public void setUrlMediumImage(String urlMediumImage) {
+        this.urlMediumImage = urlMediumImage;
     }
 }
---
+++ app/src/main/java/com/warsoft/lastfm/io/HypedArtistsDeserializer.java
@@ -0,0 +1,7 @@
+package com.warsoft.lastfm.io;
+
+/**
+ * Created by ander on 27/12/2015.
+ */
+public class HypedArtistsDeserializer {
+}
